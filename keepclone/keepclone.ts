8a67f1f8361744a2b2b1173f359aca166c724e03
jahtalab <h.jahtalab@gmail.com>
2015-11-17 21:34:26 +0100

    add auth_service fix note_service

--- keepclone/keepclone.ts
+++ keepclone/keepclone.ts
@@ -1,21 +1,29 @@
-import {Component, bootstrap} from "angular2/angular2";
+import {Component, bootstrap, NgIf} from "angular2/angular2";
 import {AddNote} from "./add_note/add_note"
 import {Notes} from "./notes/notes"
 import {NoteService} from "./note_service"
+import {AuthService} from "./auth_service"
 
 @Component({
 	selector: 'keepclone',
 	templateUrl: './keepclone/keepclone.html',
-	directives: [AddNote, Notes]
+	directives: [AddNote, Notes, NgIf]
 
 })
 
 export class KeepClone {
-	constructor() {
+	constructor(private authService: AuthService) {
 
-
-
+	}
+	public isAuthenticated(){
+		return this.authService.isAuthenticated();
+	}
+	public unAuth() {
+		this.authService.unAuth();
+	}
+	public authWithGoogle() {
+		this.authService.initGoogleAuth();
 	}
 }
 
-bootstrap(KeepClone, [NoteService]);
+bootstrap(KeepClone, [NoteService, AuthService]);
--- keepclone/note_service.ts
+++ keepclone/note_service.ts
@@ -1,14 +1,30 @@
+import {Injectable} from 'angular2/angular2';
+import {AuthService} from './auth_service'
+@Injectable()
 export class NoteService {
-	private url = "https://keepclone.firebaseio.com/notes";
+	private url = "https://keepclone.firebaseio.com";
 	private notes: Firebase;
-	constructor() {
-		this.notes = new Firebase(this.url);
+	constructor(private authService: AuthService) {
+		this.notes = new Firebase(this.url + "/notes");
 	}
 	public push(note) {
-		this.notes.push(note);
+		if (this.authService.isAuthenticated()) {
+			var notes = this.getUserNotesRef();
+			notes.push(note);
+		} else {
+			this.notes.push(note);
+		}
 	}
-
+	private getUserNotesRef() {
+		return new Firebase(this.url + '/users/' + this.authService.getUid() + '/notes');
+	}
 	public on(callback) {
-		this.notes.on('value', callback);
+		if (this.authService.isAuthenticated()) {
+			var notes = this.getUserNotesRef();
+			notes.on('value', callback);
+		} else {
+			this.notes.on('value', callback);
+		}
+
 	}
 }
