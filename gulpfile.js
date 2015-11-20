6d4fec1068355fbdbc657f9d3589325521c34093
jahtalab <h.jahtalab@gmail.com>
2015-11-20 12:55:43 +0100

    add font-awesome & add underscore

--- gulpfile.js
+++ gulpfile.js
@@ -7,32 +7,43 @@ var typescriptPath = './keepclone/**/*.ts';
 var tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('build.typescript', function (release) {
-    var tsResult = tsProject.src() 
+    var tsResult = tsProject.src()
         .pipe(ts(tsProject));
-      return tsResult.js
-          .pipe(gulp.dest('./dist'));
+    return tsResult.js
+        .pipe(gulp.dest('./dist'));
 });
 
 var viewsPath = './keepclone/**/*.html';
 gulp.task('copyviews', function (release) {
-  gulp.src(viewsPath)
-  .pipe(gulp.dest('./dist/keepclone'));
+    gulp.src(viewsPath)
+        .pipe(gulp.dest('./dist/keepclone'));
     return gulp.src('./views/index.html')
         .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('copylib', function () {
-  gulp.src('./node_modules/jquery/dist/**/*.*')
-     .pipe(gulp.dest('./dist/jquery'));
-  gulp.src('./node_modules/materialize-css/dist/**/*.*')
-     .pipe(gulp.dest('./dist/materialize-css'));
+
+    gulp.src('./node_modules/font-awesome/css/*.*')
+        .pipe(gulp.dest('./dist/font-awesome/css'));
+    gulp.src('./node_modules/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('./dist/font-awesome/fonts'));
+
+    gulp.src('./node_modules/systemjs/dist/system.js')
+        .pipe(gulp.dest('./dist/systemjs'));
+    gulp.src('./node_modules/jquery/dist/**/*.*')
+        .pipe(gulp.dest('./dist/jquery'));
+    gulp.src('./node_modules/materialize-css/dist/**/*.*')
+        .pipe(gulp.dest('./dist/materialize-css'));
+
+    gulp.src('./node_modules/underscore/underscore.js')
+        .pipe(gulp.dest('./dist/underscore'));
 
     return gulp.src('./node_modules/angular2/bundles/angular2.js')
         .pipe(gulp.dest('./dist/angular2'));
 });
 
 gulp.task('build', function () {
-    var tasks = ['build.typescript','copyviews','copylib'];
+    var tasks = ['build.typescript', 'copyviews', 'copylib'];
     runSequence(tasks);
 });
 
@@ -43,5 +54,5 @@ gulp.task('default', function () {
 gulp.task('watch', function () {
     gulp.watch(typescriptPath, ['build.typescript']);
     gulp.watch(viewsPath, ['copyviews']);
-    gulp.watch('./views/index.html',['copyviews'])
+    gulp.watch('./views/index.html', ['copyviews'])
 });
