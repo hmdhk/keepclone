9aca34ef6551505bc2efbb7d7a23859b86b08bd4
jahtalab <h.jahtalab@gmail.com>
2015-11-20 10:55:48 +0100

    fixed showing new line in notes, added Done button

--- keepclone/add_note/add_note.ts
+++ keepclone/add_note/add_note.ts
@@ -13,15 +13,17 @@ export class AddNote {
   constructor(private noteService: NoteService) {
 
   }
-  public addNote() {
-
-
+  public done() {
+    this.addNote({ text: this.text, creationDate: Date.now() });
+    this.text = '';
+  }
+  public addNote(note) {
+    this.noteService.push(note)
   }
   public keydown(e) {
     // Ctrl-Enter
     if (e.ctrlKey && e.keyCode == 13) {
-      this.noteService.push({ text: this.text, creationDate: Date.now() })
-      this.text = '';
+      this.done();
     }
 
   }
